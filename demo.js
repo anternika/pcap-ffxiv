9c4253dc173ceb8d875123db15e66ea97c3f394c
anternika <7094960+anternika@users.noreply.github.com>
2026-07-24 07:14:58 -0400

    Add more spearfish packet types

--- demo.js
+++ demo.js
@@ -30,46 +30,11 @@ function demo_fish_packet(fish_message) {
 
 function demo_spearfish_packet(spearfish_message) {
 	//console.log(spearfish_message)
+	// every packet is signed with the gathering node ID from sheet GatheringPoint
+	//console.log("gathering at node %s", spearfish_message.parsedIpcData.param1 % (1 << 16))
 
-	if (spearfish_message.parsedIpcData.param2 == 3) { // this is a "caught fish" packet
-		spearfish_id = spearfish_message.parsedIpcData.param3 // this ID is from the SpearfishingItem table
-		spearfish_size = spearfish_message.parsedIpcData.param4
-		spearfish_large = ((spearfish_message.parsedIpcData.param5 >> 1) & 1)
-		console.log('you caught a %s with size=%s...%s', spearfish_id, spearfish_size, spearfish_large ? "it's large" : "it's small")
-	} else if (spearfish_message.parsedIpcData.param2 == 2) { // the fish flee
-		// it does not trigger if you close the node early (because the fish don't flee)
-		//console.log('fish flee')
-	} else if (spearfish_message.parsedIpcData.param2 == 0) { // plays when you or the game closes the window
+	if (spearfish_message.parsedIpcData.param2 == 0) { // plays when you or the game closes the window
 		//console.log('closing window')
-	} else if (spearfish_message.parsedIpcData.param2 == 6) { // the window opens
-		// param3 has some unknown (or garbage) data in it
-		//console.log('opening window %s', spearfish_message.parsedIpcData.param3)
-	} else if (spearfish_message.parsedIpcData.param2 == 5) { // after most interactions
-		// using baited breath displays a large positive number, 
-		// possibly reflecting it decreasing the wariness HP
-		current_wariness_rate = (spearfish_message.parsedIpcData.param3 / 90000) * 100
-		//console.log('wariness mod %s%%', (spearfish_message.parsedIpcData.param3 / 90000) * 100)
-	} else if (spearfish_message.parsedIpcData.param2 == 11) { // character used a skill
-		// this field pulls from the Action table...
-		// for clarity here are pre-loaded IDs
-		skill_params = {
-			'7632': 'gig',
-			'26870': 'vital sight',
-			'26871': 'baited breath',
-			'26872': 'electric current',
-			'7906': 'veteran trade'
-		}
-		//console.log('skill used: %s', skill_params[spearfish_message.parsedIpcData.param3])
-	} else if (spearfish_message.parsedIpcData.param2 == 7) { // caught first fish at node
-		// this only plays on the first catch, not on any subsequent catches
-		// even if you go from catch chain 1 -> catch chain 0 -> back to catch chain 1
-		// no idea why
-		//console.log('first catch')
-	} else if (spearfish_message.parsedIpcData.param2 == 13) { // every time vital sight is used
-		// param3/4/5 typically say 75, no idea why
-		//console.log('vital sight used (%s %s %s)', spearfish_message.parsedIpcData.param3, spearfish_message.parsedIpcData.param4, spearfish_message.parsedIpcData.param5)
-	} else if (spearfish_message.parsedIpcData.param2 == 4) { // every time catch chain updates (good or bad)
-		//console.log('CATCH CHAIN! -> %d', spearfish_message.parsedIpcData.param3)
 	} else if (spearfish_message.parsedIpcData.param2 == 1) { // every time a new fish shadow spawns
 		// 0 = bottom lane, 1 = middle lane, 2 = top lane
 		// % (1 << 2) = get the last two bits
@@ -79,16 +44,18 @@ function demo_spearfish_packet(spearfish_message) {
 			'1': 'middle',
 			'2': 'top'
 		}
-		// speeds, slower to faster: 150, 200, 300, 350, 400
+		// speeds, slower to faster, 100 - 500 by 50
 		shadow_speed = spearfish_message.parsedIpcData.param4
 		speed_params = {
+			'100': 'basically not moving',
 			'150': 'extremely slow',
 			'200': 'very slow',
 			'250': 'slow',
 			'300': 'average',
 			'350': 'fast',
 			'400': 'very fast',
-			'500': 'extremely fast'
+			'450': 'extremely fast',
+			'500': 'too fast too furious'
 		}
 		// sizes, corresponding to old gig head sizes
 		// 1 = small, 2 = medium, 3 = large
@@ -100,9 +67,59 @@ function demo_spearfish_packet(spearfish_message) {
 		}
 		// shiny lanes have values in their upper half
 		if (spearfish_message.parsedIpcData.param3 > 3) {
-			console.log('Oooh shiny!!')
+			//console.log('Oooh shiny!!')
 		}
-		console.log('lane_%s update: speed %s, size %s', lane_params[shadow_lane], speed_params[shadow_speed], size_params[shadow_size])
+		//console.log('lane_%s update: speed %s, size %s', lane_params[shadow_lane], speed_params[shadow_speed], size_params[shadow_size])
+	} else if (spearfish_message.parsedIpcData.param2 == 2) { // the fish flee
+		// it does not trigger if you close the node early (because the fish don't flee)
+		//console.log('fish flee')
+	} else if (spearfish_message.parsedIpcData.param2 == 3) { // this is a "caught fish" packet
+		spearfish_id = spearfish_message.parsedIpcData.param3 // this ID is from the SpearfishingItem table
+		spearfish_size = spearfish_message.parsedIpcData.param4
+		spearfish_large = ((spearfish_message.parsedIpcData.param5 >> 1) & 1)
+		//console.log('you caught a %s with size=%s...%s', spearfish_id, spearfish_size, spearfish_large ? "it's large" : "it's small")
+	} else if (spearfish_message.parsedIpcData.param2 == 4) { // every time catch chain updates (good or bad)
+		//console.log('CATCH CHAIN! -> %d', spearfish_message.parsedIpcData.param3)
+	} else if (spearfish_message.parsedIpcData.param2 == 5) { // after most interactions
+		// using baited breath displays a large positive number, 
+		// possibly reflecting it decreasing the wariness HP
+		current_wariness_rate = (spearfish_message.parsedIpcData.param3 / 90000) * 100
+		//console.log('wariness mod %s%%', current_wariness_rate)
+	} else if (spearfish_message.parsedIpcData.param2 == 6) { // the window opens
+		// param3 has some unknown (or garbage) data in it, looks like a timestamp
+		//console.log('opening window %s', spearfish_message.parsedIpcData.param3)
+	} else if (spearfish_message.parsedIpcData.param2 == 7) { // caught first fish at node
+		// this only plays on the first catch, not on any subsequent catches
+		// even if you go from catch chain 1 -> catch chain 0 -> back to catch chain 1
+		// no idea why
+		//console.log('first catch')
+	} else if (spearfish_message.parsedIpcData.param2 == 8) { // intuition
+		// this is spearfish-intuition, NOT progress toward swimming shadows 'intuition'
+		// intuition ID is from SpearfishingComboTarget table
+		//console.log('intuition gained: %s', spearfish_message.parsedIpcData.param3)
+	} else if (spearfish_message.parsedIpcData.param2 == 9) { // intuition fell off naturally
+		console.log('intuition fell off: %s', spearfish_message.parsedIpcData.param3)
+	} else if (spearfish_message.parsedIpcData.param2 == 10) { // intuition removed because of catch
+		console.log('intuition consumed by catch')
+	} else if (spearfish_message.parsedIpcData.param2 == 11) { // character used a skill
+		// this field pulls from the Action table...
+		// for clarity here are pre-loaded IDs
+		skill_params = {
+			'7632': 'gig',
+			'26870': 'vital sight',
+			'26871': 'baited breath',
+			'26872': 'electric current',
+			'7906': 'veteran trade'
+		}
+		//console.log('skill used: %s', skill_params[spearfish_message.parsedIpcData.param3])
+	} else if (spearfish_message.parsedIpcData.param2 == 12) { // new record?
+		// big: 27, small: 25
+		//console.log('new record! (%s %s %s)', spearfish_message.parsedIpcData.param3, spearfish_message.parsedIpcData.param4, spearfish_message.parsedIpcData.param5)
+	} else if (spearfish_message.parsedIpcData.param2 == 13) { // every time vital sight is used
+		// param3/4/5 typically say 75, no idea why
+		//console.log('vital sight used (%s %s %s)', spearfish_message.parsedIpcData.param3, spearfish_message.parsedIpcData.param4, spearfish_message.parsedIpcData.param5)
+	} else if (spearfish_message.parsedIpcData.param2 == 14) { // intuition refreshed
+		//console.log('intuition refresh %s', spearfish_message.parsedIpcData.param3)
 	} else {
 		console.log('unmapped spearfish_message type: %s; %s,%s,%s,%s', spearfish_message.parsedIpcData.param2, spearfish_message.parsedIpcData.param3, spearfish_message.parsedIpcData.param4, spearfish_message.parsedIpcData.param5, spearfish_message.parsedIpcData.param6)
 	}
